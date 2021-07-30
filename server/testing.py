8950b7646ea83ab4a339a6b9b8e9b7bb18bfd297
Daria Razumova <d.razumova@yandex.ru>
2021-07-30 17:20:09 +0300

    Features processing

--- server/testing.py
+++ server/testing.py
@@ -1,68 +1,100 @@
-import json
+import sys
 
 import numpy as np
-from sklearn.metrics import classification_report
+from sklearn import linear_model
+import pandas as pd
 
 
-def parse_json_data(filename):
-    reports = []
-    with open(filename) as lines:
-        for line in json.loads(lines.read()):
-            reports.append(line)
-    return reports
+class LinearModel:
+    def __init__(self, n_features):
+        self.weights = np.ones(n_features + 1)
+        self.weights[-1] = 0
+
+    def set_weights(self, new_weights):
+        self.weights = new_weights
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        return np.array(X) @ self.weights[:-1] + self.weights[-1]
 
 
-def labels(report):
-    return list(map(lambda x: int(x["label"]), report["lines"]))
+def to_groups(rows):
+    ids = set(rows['reportId'])
+
+    groups = []
+    for id in ids:
+        stacktrace = rows[rows['reportId'] == id]
+        if sum(stacktrace['label']) == 0:
+            continue
+        groups.append((id, stacktrace))
+
+    return groups
 
 
-def blame_last_modified(report):
-    modification_times = np.array(list(map(lambda x: int(x["lastModified"]), report["lines"])))
-    predicted_labels = np.zeros_like(modification_times)
-    max_element = np.max(modification_times)
-    predicted_labels[modification_times == max_element] = 1
-    return predicted_labels.tolist()
+def group_to_features(stacktrace):
+    data = []
+    labels = []
+    for _, row in stacktrace.iterrows():
+        stacktrace_features = [row['isFirstLine'], row['isLastModified']]
+        data.append(stacktrace_features)
+        labels.append(row['label'])
+    return data, labels
 
 
-def blame_first_editable(report):
-    modification_times = list(map(lambda x: int(x["lastModified"]), report["lines"]))
-    predicted_labels = [0] * len(modification_times)
-    for i in range(len(modification_times)):
-        if modification_times[i] != -1:
-            predicted_labels[i] = 1
-            break
-    return predicted_labels
+def to_features(groups):
+    data = []
+    labels = []
+
+    for (id, stacktrace) in groups:
+        group_data, group_labels = group_to_features(stacktrace)
+        data += group_data
+        labels += group_labels
+    return data, labels
 
 
-def check(reports, model):
-    all_labels = []
-    all_predictions = []
-    for report in reports:
-        correct = labels(report)
-        predicted = model(report)
-        all_labels += correct
-        all_predictions += predicted
-        print("for report", report["reportId"])
-        print(correct)
-        print(predicted)
-    print(classification_report(all_labels, all_predictions))
+def count_score(groups, model, k=1):
+    cnt = 0
+    for _, stacktrace in groups:
+        y_true = stacktrace['label']
+        fetures, labels = group_to_features(stacktrace)
+        y_pred = model.predict(fetures)
+        mx_indexes = np.array(y_pred).argsort()[-k:][::-1]
+        if np.array(y_true)[mx_indexes].sum() > 0:
+            cnt += 1
+    print("score", cnt / len(groups))
 
 
 if __name__ == '__main__':
-    reports = parse_json_data("../dataset_2.json")
-    check(reports, blame_first_editable)
+    reports = sys.argv[1]
+    all_lines = pd.read_csv(reports)
+    all_lines = all_lines[all_lines['editable'] == 1]
 
+    groups = to_groups(all_lines)
+    print(len(groups))
+    np.random.shuffle(groups)
 
-""" For last modified lines with file modification time as true label
-        precision    recall  f1-score   support
+    length = len(groups)
+    coef = 0.8
+    part = int(length * coef)
 
-           0       0.96      0.93      0.94      3761
-           1       0.79      0.88      0.83      1186
-"""
+    groups_train = groups[:part]
+    groups_test = groups[part:]
 
-""" For first editable line with file modification as true label
-        precision    recall  f1-score   support
+    X_train, y_train = to_features(groups_train)
 
-           0       0.76      0.99      0.86      3761
-           1       0.56      0.03      0.06      1186
-"""
+    lin = linear_model.LinearRegression()
+    lin.fit(X_train, y_train)
+    print(lin.coef_)
+
+    count_score(groups_test, lin)
+
+    model = LinearModel(3)
+    model.set_weights(np.array([1.0, 0.0, 0.0]))
+
+    count_score(groups_test, model)
+
+    model.set_weights(np.array([1.0, 1.0, 0.0]))
+
+    count_score(groups_test, model)
