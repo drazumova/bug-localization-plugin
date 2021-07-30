730bfadc0fbe0a7bea454e753509098eadbc9c7f
Daria Razumova <d.razumova@yandex.ru>
2021-07-30 17:58:46 +0300

    Fix features.csv generating

--- src/main/kotlin/com/github/drazumova/plugintest/dataset/Features.kt
+++ src/main/kotlin/com/github/drazumova/plugintest/dataset/Features.kt
@@ -3,6 +3,7 @@ package com.github.drazumova.plugintest.dataset
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import org.jetbrains.kotlin.idea.configuration.fetchGradleVersion
 import java.io.File
 import java.nio.file.InvalidPathException
 
@@ -14,13 +15,14 @@ data class Features(
     val editable: Int
 )
 
-internal fun Features.toList(reportId: String): List<String> {
+internal fun Features.toList(reportId: String, label: Int): List<String> {
     return listOf(
         reportId,
         lineNumber.toString(),
         isFirstLine.toString(),
         isLastModified.toString(),
-        editable.toString()
+        editable.toString(),
+        label.toString()
     )
 }
 
@@ -37,7 +39,6 @@ fun Boolean.toInt() = if (this) 1 else 0
 fun features(lines: List<FullStacktraceLineInformation>): List<Features> {
     val methodModificationTimes = lines.map {
         val methodRange = methodTextRange(it.fileText, it.method)
-//        println("${it.fileText} $methodRange")
         val methodModificationTime =
             if (methodRange.first != -1)
                 it.annotation?.lineAnnotation
@@ -60,29 +61,33 @@ fun features(lines: List<FullStacktraceLineInformation>): List<Features> {
     }
 }
 
-fun lineFromReportDirectory(directory: File): List<Features>? {
+fun lineFromReportDirectory(directory: File): Pair<List<Features>, List<Int>>? {
     val files = directory.listFiles() ?: return null
     val reportFile = files.singleOrNull { file -> file.extension == "json" } ?: return null
     val filesDirectory = files.singleOrNull { file -> file.isDirectory } ?: return null
 
     val report = Json.decodeFromString<Report>(reportFile.readText())
     val lines = report.lines.map {
-        val reportDirectory = File(filesDirectory, it.filename)
-        val reportLineFile = File(File(reportDirectory, "files"), it.filename)
-        if (!reportDirectory.exists() || !reportLineFile.exists())
+        if (it.filename.isEmpty()) return@map FullStacktraceLineInformation(
+            it.line, it.filename, it.methodName, null, ""
+        )
+        val lineFile = File(filesDirectory, it.filename)
+        if (!lineFile.exists()) {
             return@map FullStacktraceLineInformation(
                 it.line, it.filename, it.methodName, null, ""
             )
+        }
 
-        val fileText = reportLineFile.readText()
+        val fileText = lineFile.readText()
 
         val savedAnnotation = File(filesDirectory, "${it.filename}.json")
         val annotatedFile = Json.decodeFromString<AnnotatedFile?>(savedAnnotation.readText())
 
         FullStacktraceLineInformation(it.line, it.filename, it.methodName, annotatedFile, fileText)
     }
+    val labels = report.lines.map { it.label }
 
-    return features(lines)
+    return Pair(features(lines), labels)
 }
 
 fun main(args: Array<String>) {
@@ -97,16 +102,16 @@ fun main(args: Array<String>) {
         throw InvalidPathException(inputPath, "Empty or broken files directory")
 
     val features = directory.listFiles()?.flatMap { file ->
-        val reportLines = lineFromReportDirectory(file) ?: emptyList()
-        println(file.name)
-        reportLines.map { it.toList(file.name) }
+        val (features, labels) = lineFromReportDirectory(file) ?: return@flatMap emptyList()
+
+        features.zip(labels).map { (features: Features, label: Int) -> features.toList(file.name, label) }
     } ?: emptyList()
 
 
     val columns = Features::class.java.declaredFields.map { it.name }
 
-    csvWriter().open("test_2.csv") {
-        writeRow(listOf("reportId").plus(columns))
+    csvWriter().open(outputFile) {
+        writeRow(listOf("reportId").plus(columns).plus("label"))
         writeRows(features)
     }
 }
